3047072a9b1ed36feca031da30a05a9d1da6dce7
BlobTheKat <blob.kat@hotmail.com>
2024-11-14 17:48:14 +0000

    default title and icon

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-/*const i = TextField()
+const i = TextField()
 
 render = () => {
 	ctx.reset(1/ctx.width, 0, 0, 1/ctx.height, .5, .5)
 	
-}*/
-const img = Img('./creo5.png', REPEAT_MIRRORED)
-render = () => {
-	ctx.drawRect(0, 0, 1, 1, img)
 }
